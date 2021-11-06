74c81f1211b71c4fc26a85003ea39324495e5c0b
WathikAhmed <zerox4224@gmail.com>
2021-11-06 16:07:27 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 8080;
+const PORT = 8090;
 const express = require('express');     //  Framework that provides a robust set of features for web and mobile applications. Creating a robust API is quick and easy
 const cheerio = require('cheerio');     //  Cheerio parses markup and provides an API for traversing/manipulating the resulting data structure.
 const axios = require('axios');         //  Promise based HTTP client for the browser and node.js
@@ -16,11 +16,15 @@ app.listen(
 );
 
 app.get('/', (req, res) => {
-    res.json('Welcome to the Stake API')
     axios.get(address).then(response => {
         const html = response.data
         const $ = cheerio.load(html)
-        res.sendFile($);
+        
+        $('h3:contains("US")', html).each(function () {
+            const equityValue = $(this).text()
+            res.json(equityValue)
+        })
+        res.json("Welcome to Stake API")
     })
 });
 
